2507b206e4fecb20ff300158991adf4fc5fd30af
razafimanantsoaamrah-droid <razafimanantsoaamrah@gmail.com>
2026-03-14 10:55:23 +0100

    feat: add cron service to reload service

---
+++ srcs/config/cron.js
@@ -0,0 +1,24 @@
+import cron from 'node-cron';
+import https from 'https';
+import { error } from 'console';
+
+const job = new cron.CronJob("*/14 * * * *", function() {
+  https
+  .get(process.env.CRON_URL, (res) => {
+    if (res.statusCode === 200) {
+      console.log("Cron job executed successfully");
+    }
+    console.log(`Status Code: ${res.statusCode}`);
+  })
+  .on("error", (e) => console.error(`Error executing cron job: ${e.message}`));
+});
+
+export default job;
+
+// The cron job is scheduled to run every 14 minutes,*
+// making a GET request 
+// to the specified URL. If the request is 
+// successful (status code 200), it logs a 
+// success message. If there's an error 
+// during the request,
+//  it logs the error message.
