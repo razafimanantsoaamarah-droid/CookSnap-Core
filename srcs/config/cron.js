16e00222cd19ea622d16a0cbf558bd4fec9e2b93
razafimanantsoaamrah-droid <razafimanantsoaamrah@gmail.com>
2026-03-14 11:03:03 +0100

    remove node module in the origin

--- srcs/config/cron.js
+++ srcs/config/cron.js
@@ -4,7 +4,7 @@ import { error } from 'console';
 
 const job = new cron.CronJob("*/14 * * * *", function() {
   https
-  .get(process.env.CRON_URL, (res) => {
+  .get(process.env.API_URL, (res) => {
     if (res.statusCode === 200) {
       console.log("Cron job executed successfully");
     }
